0120b42e388306f87cf56cb53f78b2b9a8e7323b
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2024-11-25 15:41:36 +0100

    Improvement

---
+++ classes/output/checkoutfail.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains the definition for the renderable classes for the booking instance
+ *
+ * @package   paygw_unisbg
+ * @copyright 2021 Georg Maißer {@link http://www.wunderbyte.at}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace paygw_unisbg\output;
+
+use renderer_base;
+use renderable;
+use templatable;
+
+/**
+ * This class prepares data for displaying a booking option instance
+ *
+ * @package paygw_unisbg
+ * @copyright 2022 Georg Maißer {@link http://www.wunderbyte.at}
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class checkoutfail implements renderable, templatable {
+
+    /** @var array $checkoutid */
+    public $data = [];
+
+    /**
+     * In the Constructor, we gather all the data we need ans store it in the data property.
+     */
+    public function __construct() {
+        $this->data['success'] = false;
+        $this->data['msg'] = get_string('paymentfail', 'paygw_unisbg');
+    }
+
+    /**
+     * Export data for checkout template.
+     * @param renderer_base $output
+     * @return array
+     */
+    public function export_for_template(renderer_base $output) {
+        return $this->data;
+    }
+}
---
+++ success_notify.php
@@ -0,0 +1,52 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Settings for the unisbg payment gateway
+ *
+ * @package    paygw_unisbg
+ * @copyright  2022 Wunderbyte Gmbh <info@wunderbyte.at>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$encryptedpayload = optional_param('zahlungsdetails', '', PARAM_RAW);
+
+// Todo: decrypt
+
+// todo: via tid, return $itemmid & $userid from unisbg_openorderstable
+
+
+
+
+
+// Todo call moodle webservice on result SUCCESS
+
+$notifyurl = new \moodle_url(
+  '/webservice/rest/server.php',
+  [
+      'wsfunction' => 'local_shopping_cart_verify_purchase',
+      'wstoken' => get_config('paygw_unisbg', 'tokenforverification'),
+      'moodlewsrestformat' => 'json',
+      'identifier' => $itemid,
+      'tid' => $cartid,
+      'paymentgateway' => 'unisbg',
+      'userid' => $userdata->id,
+  ]
+);
+
+redirect($notifyurl->out());
