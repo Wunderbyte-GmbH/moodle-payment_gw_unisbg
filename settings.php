e06c07cb58a48e7f2811f6cebc3184db372fce0a
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2024-12-11 11:19:31 +0100

    Improvement: removed access token from settings

--- settings.php
+++ settings.php
@@ -26,15 +26,5 @@ defined('MOODLE_INTERNAL') || die();
 
 if ($ADMIN->fulltree) {
     $settings->add(new admin_setting_heading('paygw_unisbg_settings', '', get_string('pluginname_desc', 'paygw_unisbg')));
-
-    $settings->add(new admin_setting_configtext(
-        'paygw_unisbg/tokenforverification',
-        get_string('tokenforverification', 'paygw_unisbg'),
-        '',
-        '',
-        PARAM_TEXT
-        )
-    );
-
     \core_payment\helper::add_common_gateway_settings($settings, 'paygw_unisbg');
 }
